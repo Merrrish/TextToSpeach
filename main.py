192473fd0c0538748c388d8ddfc368b6e64d2774
Merrrish <isbro4217@gmail.com>
2025-02-11 19:42:19 +0100

    Обновил main.py: загрузка токена из .env

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from aiogram import Bot, Dispatcher
 import asyncio
 from datetime import datetime
-from App.config import TOKEN
+import os
+from dotenv import load_dotenv
 from App.handlers import router
 
+# Загружаем переменные из .env
+load_dotenv()
 
-token = TOKEN
-bot = Bot(token)
+# Получаем токен из переменных окружения
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(TOKEN)
 dp = Dispatcher()
 
 async def main():
     dp.include_router(router)
     print(f"Время запуска кода: {datetime.now()}")
     await dp.start_polling(bot)
-    
-    
+
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Программа была завершена: {datetime.now()}")
+        print(f"Программа была завершена: {datetime.now()}")
